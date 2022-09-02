838def3cda53887945ab47a713e454130a0ff0df
Jason Jean <jasonjean1993@gmail.com>
2022-09-02 14:03:44 -0400

    repro

---
+++ libs/lib1/src/lib/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-test-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css123123'],
+})
+export class ButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
