8649f76b25e28abc008a18ca7650cee03e62c082
FrozenPandaz <jasonjean1993@gmail.com>
2023-02-07 16:38:52 -0500

    repro incremental build issue

--- apps/app1/src/app/app.module.ts
+++ apps/app1/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import {ButtonsModule} from "@inc-buildable-test/buttons";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ButtonsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
