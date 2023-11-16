4dd049d719ed3780fdc23586ee2bf94d866ec637
FrozenPandaz <jasonjean1993@gmail.com>
2023-11-16 12:59:16 -0500

    change

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -5,6 +5,7 @@ describe('e2e', () => {
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
+    // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
